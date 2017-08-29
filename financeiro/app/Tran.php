3a7c002635870f2685b902f0c31a3e6a8c8806a9
cayque16 <cayque_santos16@hotmail.com>
2017-08-28 22:07:08 -0300

    P2

--- financeiro/app/Tran.php
+++ financeiro/app/Tran.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tran extends Model
 {
     protected $fillable = ['user_id','tipo_id','data','valor','credito'];
+
+    public function Tipo(){
+      return $this->belongsTo('App\Tipo');
+    }
+
+    public function User(){
+      return $this->belongsTo('App\User');
+    }
 }
